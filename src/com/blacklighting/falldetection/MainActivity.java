975fd69a89be8e7247e8ae1e1638074f6aa70652
liuyajun <liuyajun@localhost>
2014-02-20 17:43:44 +0800

    增加收集地理位置信息，发送求助短信，但是还没有成功

--- src/com/blacklighting/falldetection/MainActivity.java
+++ src/com/blacklighting/falldetection/MainActivity.java
@@ -1,55 +1,20 @@
 package com.blacklighting.falldetection;
 
-import android.app.AlertDialog.Builder;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceManager;
-import android.view.KeyEvent;
 import android.view.Menu;
-import android.widget.Toast;
 
 public class MainActivity extends PreferenceActivity {
-	boolean hasHeightSeting, hasPositionSeting, hasPhoneSeting,
-			hasRingtoneSeting;
 
 	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.setting);
-		SharedPreferences sp = PreferenceManager
-				.getDefaultSharedPreferences(getApplicationContext());
 		Intent i=new Intent(MainActivity.this,DetectionServer.class);
 		startService(i);
 
-		sp.registerOnSharedPreferenceChangeListener(new OnSharedPreferenceChangeListener() {
-
-			@Override
-			public void onSharedPreferenceChanged(SharedPreferences arg0,
-					String arg1) {
-				if (arg1.equals("height")) {
-					hasHeightSeting = true;
-				} else if (arg1.equals("position")) {
-					hasPositionSeting = true;
-				} else if (arg1.equals("phone")) {
-					hasPhoneSeting = true;
-				} else if (arg1.equals("ringtone")) {
-					hasRingtoneSeting = true;
-				} 
-
-				if (hasHeightSeting && hasPositionSeting && hasPhoneSeting
-						&& hasRingtoneSeting) {
-					Toast.makeText(getApplicationContext(), "设置完成，检测服务启动",
-							Toast.LENGTH_SHORT).show();
-					startService(new Intent(MainActivity.this,
-							DetectionServer.class));
-				}
-			}
-		});
 	}
 
 	@Override
@@ -59,31 +24,6 @@ public class MainActivity extends PreferenceActivity {
 		return true;
 	}
 
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_BACK) {
-			if (!(hasHeightSeting && hasPositionSeting && hasPhoneSeting
-					&& hasRingtoneSeting )) {
-				Builder builder = new Builder(MainActivity.this);
-				builder.setTitle("提示")
-						.setMessage("还未完成设置，退出后检测服务将不会启动，确定要退出吗？")
-						.setNegativeButton("取消", null)
-						.setPositiveButton("确认",
-								new DialogInterface.OnClickListener() {
 
-									@Override
-									public void onClick(DialogInterface arg0,
-											int arg1) {
-										finish();
-									}
-								});
-				builder.create().show();
-			}else{
-				finish();
-			}
-			return false;
-		}
-		return super.onKeyDown(keyCode, event);
-	}
 
 }
--- src/com/blacklighting/falldetection/WarmingActivity.java
+++ src/com/blacklighting/falldetection/WarmingActivity.java
@@ -1,25 +1,57 @@
 package com.blacklighting.falldetection;
 
+import java.lang.ref.WeakReference;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.PreferenceManager;
 import android.app.Activity;
+import android.content.Context;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
 
-public class WarmingActivity extends Activity {
+public class WarmingActivity extends Activity implements OnClickListener,
+		LocationListener {
+	private LocationManager locationManager;
+	private Location currentLocation;
+	private SmsManager smsManager;
+	private MHandler mHandler;
+	private String phoneNumber;
+	private String smsContent = "软件检测到的老人发生了跌倒，请尽快采取措施，位置：";
+	private TextView counterView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_warming);
-		findViewById(R.id.okButton).setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				finish();
-			}
-		});
+		counterView = (TextView) findViewById(R.id.counter);
+
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		smsManager = SmsManager.getDefault();
+		mHandler = new MHandler(this);
+		phoneNumber = PreferenceManager.getDefaultSharedPreferences(
+				getApplicationContext()).getString("phone", null);
+		findViewById(R.id.concleButton).setOnClickListener(this);
+
+		// 注册位置监听器，开始收集地理位置信息
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				1000, 0, WarmingActivity.this);
+
+		new CountThread().start();
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		locationManager.removeUpdates(WarmingActivity.this);
 	}
 
 	@Override
@@ -27,6 +59,93 @@ public class WarmingActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.warming, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+
+	class CountThread extends Thread {
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			super.run();
+
+			// 倒数一分钟
+			for (int i = 0; i < 60; i++) {
+				if (isInterrupted()) {
+					return;
+				}
+				try {
+					sleep(1000);
+					mHandler.sendEmptyMessage(60 - i);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			// 发送求助短信
+			if (phoneNumber != null && currentLocation != null) {
+				smsManager.sendTextMessage(phoneNumber, null, smsContent
+						+ currentLocation.toString(), null, null);
+			}
+			mHandler.sendEmptyMessage(-1);
+		}
+
+		@Override
+		public void interrupt() {
+			// TODO Auto-generated method stub
+			super.interrupt();
+			mHandler.sendEmptyMessage(-1);
+		}
+
+	}
+
+	static class MHandler extends Handler {
+		private WeakReference<WarmingActivity> act;
+
+		public MHandler(WarmingActivity act) {
+			this.act = new WeakReference<WarmingActivity>(act);
+		}
+
+		@Override
+		public void handleMessage(Message msg) {
+			// TODO Auto-generated method stub
+			super.handleMessage(msg);
+			if (msg.what == -1) {
+				act.get().finish();
+			} else {
+				act.get().counterView.setText(msg.what);
+			}
+		}
+
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		currentLocation = location;
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
