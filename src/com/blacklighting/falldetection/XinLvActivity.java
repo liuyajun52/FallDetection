5824c0ab2838f9d9deb8eddcab840a513421b2e2
liuyajun <liuyajun52@gmail.com>
2014-09-13 09:34:14 +0800

    增加了体温检测页面，心率检测页面

--- src/com/blacklighting/falldetection/XinLvActivity.java
+++ src/com/blacklighting/falldetection/XinLvActivity.java
@@ -3,36 +3,32 @@ package com.blacklighting.falldetection;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Toast;
 import android.os.Build;
 
-public class ShouhuanActivity extends ActionBarActivity {
+public class XinLvActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_shouhuan);
+		setContentView(R.layout.activity_xin_lv);
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
-
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.shouhuan, menu);
+		getMenuInflater().inflate(R.menu.xin_lv, menu);
 		return true;
 	}
 
@@ -51,8 +47,7 @@ public class ShouhuanActivity extends ActionBarActivity {
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
-	public static class PlaceholderFragment extends Fragment implements
-			OnClickListener {
+	public static class PlaceholderFragment extends Fragment {
 
 		public PlaceholderFragment() {
 		}
@@ -60,35 +55,9 @@ public class ShouhuanActivity extends ActionBarActivity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_shouhuan,
+			View rootView = inflater.inflate(R.layout.fragment_xin_lv,
 					container, false);
-			rootView.findViewById(R.id.falldecButton).setOnClickListener(this);
-			rootView.findViewById(R.id.heartButton).setOnClickListener(this);
-			rootView.findViewById(R.id.heatButton).setOnClickListener(this);
-			rootView.findViewById(R.id.heathButton).setOnClickListener(this);
 			return rootView;
-		}
-
-		@Override
-		public void onClick(View arg0) {
-			// TODO Auto-generated method stub
-			switch (arg0.getId()) {
-			case R.id.falldecButton:
-				Intent falldec = new Intent(getActivity(), MainActivity.class);
-				getActivity().startActivity(falldec);
-				break;
-			case R.id.heartButton:
-				Intent heart = new Intent(getActivity(), XinLvActivity.class);
-				getActivity().startActivity(heart);
-				break;
-			case R.id.heatButton:
-				Intent heat = new Intent(getActivity(), WenduActivity.class);
-				getActivity().startActivity(heat);
-				break;
-			case R.id.heathButton:
-				Toast.makeText(getActivity(), "亲，这里还没完成哦", Toast.LENGTH_SHORT).show();
-				break;
-			}
 		}
 	}
 
